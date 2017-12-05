2784bcfed94ee9bdb78e2b8c5719c61a57b7b8ef
Pablo Fernandez <pablo.fernandez@cscs.ch>
2017-12-05 10:00:59 +0100

    redirect by default, plus scoped token request testing

--- keystone_babel.py
+++ keystone_babel.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 
@@ -11,12 +11,12 @@ from keystoneauth1 import session
 
 app = flask.Flask(__name__)
 
-OS_AUTH_URL = 'https://pollux.cscs.ch:13000/v3'
+REAL_KEYSTONE = 'https://pollux-tds.cscs.ch:13000'
+OS_AUTH_URL = 'https://pollux-tds.cscs.ch:13000/v3'
 OS_IDENTITY_PROVIDER = 'cscskc'
-OS_IDENTITY_PROVIDER_URL = 'https://kc.cscs.ch/auth/realms/cscs/protocol/saml/'
+OS_IDENTITY_PROVIDER_URL = 'https://kc-tds.cscs.ch/auth/realms/cscs/protocol/saml/'
 OS_PROTOCOL = 'mapped'
 OS_INTERFACE = 'public'
-
 
 # TODO implement / forward entire keystone API
 # https://developer.openstack.org/api-ref/identity/v3/
@@ -25,9 +25,21 @@ OS_INTERFACE = 'public'
 #===============================================================================
 @app.route('/v3/auth/tokens', methods=['POST'])
 def tokens():
-
+    print "Requested %s: %s", flask.request.method, '/v3/auth/tokens'
     # parse request
     body = flask.request.get_json()
+    headers = flask.request.headers
+    print "Body: ", body
+    print "Headers: ", headers
+    # Bypass requests without a password inside (e.g. for unscoped-to-scoped auth)
+    if 'password' not in body['auth']['identity']:
+        print "proxying the non-password request to the real keystone"
+        #r = requests.post(REAL_KEYSTONE+"/v3/auth/token", json=body, headers={'X-Auth-Token':headers['X-Auth-Token']})
+        r = requests.post(REAL_KEYSTONE+"/v3/auth/token", json=body, headers=headers)
+        print "Response: ", r.status_code, r.headers, r.text
+        return flask.Response(r.text, headers=dict(r.headers), status=r.status_code)
+        #print "redirecting to: "+REAL_KEYSTONE+"/v3/auth/token"
+        #return flask.redirect (REAL_KEYSTONE+"/v3/auth/token")
     user = body['auth']['identity']['password']['user']
     username = user['name']
     password = user['password']
@@ -48,15 +60,40 @@ def tokens():
                                 "token": {"id": unscoped_token}}
 
     # get scoped token
-    r = requests.post(OS_AUTH_URL+'/auth/tokens', json=body)
+    r = requests.post(OS_AUTH_URL+'/auth/tokens', json=body, headers=headers)
 
+    print "Response: ", r.status_code, r.headers, r.text
+    
     # TODO: maybe patch service catalog?
 
     # forward response
     return flask.Response(r.text, headers=dict(r.headers), status=r.status_code)
 
 #===============================================================================
+@app.route('/<path:url>', methods=['POST','GET'])
+def other(url):
+    print "Requested %s: %s", flask.request.method, url
+    body = flask.request.get_json()
+    headers = flask.request.headers
+    print "Body: ", body
+    print "Headers: ", headers
+#    body = flask.request.get_json()
+#    app.logger.info("Body %s", body)
+#    if flask.request.method == 'POST':
+#      r = requests.post(REAL_KEYSTONE+url, json=body)
+#    else:
+#      app.logger.info("Redirecting to: %s", OS_AUTH_URL+url)
+#      return flask.redirect (REAL_KEYSTONE+url)
+#      app.logger.info("Args %s", flask.request.args)
+#      app.logger.info("Headers %s", flask.request.headers)
+#      req_url = OS_AUTH_URL+"/{}?{}".format(url, flask.request.query_string.decode('utf-8'))
+#      app.logger.info("Target %s", req_url)
+#      r = requests.get(req_url, headers=flask.request.headers, stream=True)
+#    return flask.Response(r.text, headers=dict(r.headers), status=r.status_code)
+    return flask.redirect (REAL_KEYSTONE+'/'+url)
+
+#===============================================================================
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(port=13000, debug=True)
 
 #EOF
