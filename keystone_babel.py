3694956d0604b31ed2b4f820cd5e771799d2af9b
Pablo Fernandez <pablo.fernandez@cscs.ch>
2017-12-06 07:18:50 +0100

    the proxy works for SSL as well now. Missing v2 auth

--- keystone_babel.py
+++ keystone_babel.py
@@ -5,47 +5,62 @@
 import flask
 import requests
 
+from flask import request
+
 from keystoneauth1.extras._saml2 import V3Saml2Password
 from keystoneauth1.identity.v3 import Token
 from keystoneauth1 import session
+from urlparse import urlsplit, urlunsplit
 
 app = flask.Flask(__name__)
 
-REAL_KEYSTONE = 'https://pollux-tds.cscs.ch:13000'
-OS_AUTH_URL = 'https://pollux-tds.cscs.ch:13000/v3'
+REAL_KEYSTONE = 'pollux-tds.cscs.ch:13000'
 OS_IDENTITY_PROVIDER = 'cscskc'
 OS_IDENTITY_PROVIDER_URL = 'https://kc-tds.cscs.ch/auth/realms/cscs/protocol/saml/'
 OS_PROTOCOL = 'mapped'
 OS_INTERFACE = 'public'
+enable_ssl = False
 
-# TODO implement / forward entire keystone API
-# https://developer.openstack.org/api-ref/identity/v3/
+def proxy():
+    # replace url with that of the real keystone
+    spliturl=list(urlsplit(request.url))
+    spliturl[0] = 'https'  # the real keystone is most likely behind SSL
+    spliturl[1] = REAL_KEYSTONE
+    url = urlunsplit(spliturl)
+
+    # do the request
+    resp = requests.request(
+        method=request.method,
+	#url=request.url.replace(FAKE_KEYSTONE, REAL_KEYSTONE),
+        url=url,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
+
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
+
+    return flask.Response(resp.content, resp.status_code, headers)
 
 
 #===============================================================================
 @app.route('/v3/auth/tokens', methods=['POST'])
 def tokens():
-    print "Requested %s: %s", flask.request.method, '/v3/auth/tokens'
     # parse request
     body = flask.request.get_json()
     headers = flask.request.headers
-    print "Body: ", body
-    print "Headers: ", headers
+
     # Bypass requests without a password inside (e.g. for unscoped-to-scoped auth)
     if 'password' not in body['auth']['identity']:
-        print "proxying the non-password request to the real keystone"
-        #r = requests.post(REAL_KEYSTONE+"/v3/auth/token", json=body, headers={'X-Auth-Token':headers['X-Auth-Token']})
-        r = requests.post(REAL_KEYSTONE+"/v3/auth/token", json=body, headers=headers)
-        print "Response: ", r.status_code, r.headers, r.text
-        return flask.Response(r.text, headers=dict(r.headers), status=r.status_code)
-        #print "redirecting to: "+REAL_KEYSTONE+"/v3/auth/token"
-        #return flask.redirect (REAL_KEYSTONE+"/v3/auth/token")
+        return proxy()
     user = body['auth']['identity']['password']['user']
     username = user['name']
     password = user['password']
 
     # get unscoped token via SAML
-    auth = V3Saml2Password(auth_url=OS_AUTH_URL,
+    auth = V3Saml2Password(auth_url='https://'+REAL_KEYSTONE+'/v3',
                            identity_provider=OS_IDENTITY_PROVIDER,
                            protocol=OS_PROTOCOL,
                            identity_provider_url=OS_IDENTITY_PROVIDER_URL,
@@ -60,40 +75,25 @@ def tokens():
                                 "token": {"id": unscoped_token}}
 
     # get scoped token
-    r = requests.post(OS_AUTH_URL+'/auth/tokens', json=body, headers=headers)
-
-    print "Response: ", r.status_code, r.headers, r.text
-    
-    # TODO: maybe patch service catalog?
+    r = requests.post('https://'+REAL_KEYSTONE+'/v3/auth/tokens', json=body, headers=headers)
 
     # forward response
     return flask.Response(r.text, headers=dict(r.headers), status=r.status_code)
 
+# Do we need to implement / forward entire keystone API? Let's hope this is enough:
+# https://developer.openstack.org/api-ref/identity/v3/
 #===============================================================================
 @app.route('/<path:url>', methods=['POST','GET'])
 def other(url):
-    print "Requested %s: %s", flask.request.method, url
-    body = flask.request.get_json()
-    headers = flask.request.headers
-    print "Body: ", body
-    print "Headers: ", headers
-#    body = flask.request.get_json()
-#    app.logger.info("Body %s", body)
-#    if flask.request.method == 'POST':
-#      r = requests.post(REAL_KEYSTONE+url, json=body)
-#    else:
-#      app.logger.info("Redirecting to: %s", OS_AUTH_URL+url)
-#      return flask.redirect (REAL_KEYSTONE+url)
-#      app.logger.info("Args %s", flask.request.args)
-#      app.logger.info("Headers %s", flask.request.headers)
-#      req_url = OS_AUTH_URL+"/{}?{}".format(url, flask.request.query_string.decode('utf-8'))
-#      app.logger.info("Target %s", req_url)
-#      r = requests.get(req_url, headers=flask.request.headers, stream=True)
-#    return flask.Response(r.text, headers=dict(r.headers), status=r.status_code)
-    return flask.redirect (REAL_KEYSTONE+'/'+url)
+    return proxy()
+
+# TODO: do we need to patch service catalog? Let's hope not!
 
 #===============================================================================
 if __name__ == "__main__":
-    app.run(port=13000, debug=True)
-
+    if enable_ssl:
+        #app.run(host='0.0.0.0', port=13000, ssl_context=('yourserver.crt', 'yourserver.key'), debug=True)
+        app.run(host='0.0.0.0', port=13000, ssl_context='adhoc', debug=True)
+    else:
+        app.run(host='0.0.0.0', port=13000, debug=True)
 #EOF
